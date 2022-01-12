054b2f1db7d2f8d35d03f79fb5921364c90ea8ef
lvyuru <lvyuru@58.com>
2022-01-13 00:12:24 +0800

    第六章

--- chapter1/helloworld/main/helloworld.go
+++ chapter1/helloworld/main/helloworld.go
@@ -1,5 +1,10 @@
 package main
 
+import "fmt"
+
 func main() {
-	println("Hello world")
+	for i, j, s := 0, 5, "a"; i < 3 && j < 100 && s != "aaaaa"; i, j,
+		s = i+1, j+1, s+"a" {
+		fmt.Println("Value of i, j, s:", i, j, s)
+	}
 }
